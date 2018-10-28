0ab568e526d068415ecbee451493df106f52bcc6
Ireneusz Larysz <ireneuszlarysz@gmail.com>
2018-10-28 22:34:09 +0100

    First chapter done

--- Part 1 - Data Preprocessing/my_data_preprocessing.R
+++ Part 1 - Data Preprocessing/my_data_preprocessing.R
@@ -1,2 +1,34 @@
-a <- c(1,2,3)
-print(a[2])
+setwd("C:/Users/Ireneusz Larysz/Downloads/Python/Machine_Learning/Part 1 - Data Preprocessing")
+dataset <- read.csv("Data.csv")
+
+# Fill nan with mean of each column
+dataset$Age <- ifelse(is.na(dataset$Age),
+    ave(dataset$Age, FUN = function(x) mean(x, na.rm=TRUE)),
+    dataset$Age)
+
+dataset$Salary <- ifelse(is.na(dataset$Salary),
+    ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+    dataset$Salary)
+
+# Category data will is going encoded as 0,1
+# According to country column a "One Hot Encoding" will be used
+dataset$Country <- factor(dataset$Country,
+                          levels = c("France", "Spain", "Germany"),
+                          labels = c(1,2,3))
+
+dataset$Purchased <- factor(dataset$Purchased,
+                            levels = c("No", "Yes"),
+                            labels = c(0,1))
+
+# Split dataset into training and test set
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# training_set = dataset$Purchased[split]
+# test_set = dataset$Purchased[!split]
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Standardize values in the set
+# Exclude categories, factors are not considered as numeric values
+training_set[,2:3] = scale(training_set[,s2:3])
+test_set[,2:3] = scale(test_set[,2:3])
--- Part 1 - Data Preprocessing/my_data_preprocessing.py
+++ Part 1 - Data Preprocessing/my_data_preprocessing.py
@@ -1,2 +1,45 @@
-print("foobar")
-print("baz")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from time import sleep
+
+# Make all array visible
+np.set_printoptions(threshold=np.nan)
+
+# Read the dataset from file
+dataset = pd.read_csv("Data.csv")
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Overwrite missing variables with mean of whole column
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer.fit(x[:,1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# LabelEncoder given subsequent numbers to every next categorical value
+# labelencoder_x = LabelEncoder()
+# x[:,0] = labelencoder_x.fit_transform(x[:,0])
+
+# This can cause problems during ML
+# That's why it is  better to use OneHotEncoder and create columns that will be only  notify about category presence
+countries = np.unique(x[:, 0])
+enc = OneHotEncoder(categories=[countries])
+temp = enc.fit_transform(x[:,0].reshape(-1, 1)).toarray()
+x = np.append(temp, x[:,1:], axis=1)
+
+# Encode yes/no array with 0,1
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Split x & y arrays in two sets, train test will contain 80% of each array
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Standardize the variables
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+sleep(1)
